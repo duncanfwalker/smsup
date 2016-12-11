d5883da0718f6605219a4435346f4fa0e77c7446
Duncan Walker <duncfw@gmail.com>
2016-12-11 21:57:03 +0000

    Add ip filtering to nexmo mo endpoint

--- server/api.js
+++ server/api.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
 const distributor = require('./subscription/distributor');
 const nexmo = require('././gateways/nexmo');
+const ipfilter = require('express-ipfilter').IpFilter;
+
+
+const nexmoIps = ['174.37.245.32/29', '174.36.197.192/28', '173.193.199.16/28', '173.193.199.16/28'];
+const localIps = ['127.0.0.1', '::1'];
+router.use(ipfilter([...nexmoIps, ...localIps], { mode: 'allow' }));
 
 // TODO: extract this to gateways
 router.post('/mo/nexmo', (req, res) => {
