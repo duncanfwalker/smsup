53969228684bec5563c822241e6e576ad45ae940
Duncan Walker <duncfw@gmail.com>
2017-05-21 19:00:45 +0100

    Add rejoin instructions to leave autoreply

--- server/views/auto-replies.js
+++ server/views/auto-replies.js
@@ -3,7 +3,10 @@ const autoReplies = {
     __('You have joined the {{groupName}} group.', viewModel),
     viewModel.includeTerms ? __('SMS Up terms of use are.', viewModel) : '',
   ]),
-  leave: (viewModel, { __ }) => __('You have left the {{groupName}} group.', viewModel),
+  leave: (viewModel, { __, join }) => join([
+    __('You have left the {{groupName}} group.', viewModel),
+    __('Reply "join {{groupName}}" to rejoin', viewModel),
+  ]),
   create: (viewModel, { __ }) => __('\'{{groupName}}\' group created. ', viewModel),
   delete: (viewModel, { __ }) => __('the {{groupName}} group has been deleted.', viewModel),
   invitation: (viewModel, { __, join }) => join([
