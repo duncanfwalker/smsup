f1317130afdc7866fc3747c9dbc8172dc186a888
Duncan Walker <duncfw@gmail.com>
2017-07-07 22:52:07 +0100

    Allow admin token reuse

--- server/admin/admin-auth.js
+++ server/admin/admin-auth.js
@@ -17,7 +17,7 @@ const loginForm = `<html>
 </html>
 `;
 
-passwordless.init(new MongoStore(process.env.MONGODBURI));
+passwordless.init(new MongoStore(process.env.MONGODBURI), { allowTokenReuse: true });
 // Set up a delivery service
 passwordless.addDelivery(
   (tokenToSend, uidToSend, recipient, callback) => {
